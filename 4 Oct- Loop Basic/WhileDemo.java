d199ea431b6400e1e82ec7b800455023e3b06175
javabyvinti <32234213+javabyvinti@users.noreply.github.com>
2017-10-05 20:09:53 +0530

    Add files via upload

---
+++ 4 Oct- Loop Basic/WhileDemo.java
@@ -0,0 +1,29 @@
+package com.bmpl.javaprograms;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		
+		//initialization
+		int i = 1;
+
+		for(; i<=10; i = i + 2);
+		
+		{//block--> type
+			System.out.println(i);
+		}
+		
+//		do{
+//			System.out.println(i);
+//			i--;
+//		}while(i>=11);
+		
+//		while(i>=1){
+//			System.out.println(i);
+//			//i--;
+//		}
+		
+
+	}
+
+}
